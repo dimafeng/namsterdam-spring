7742b8e25fb9a08068d827de521043429802e082
d.fedosov <d.fedosov@fundcount.com>
2014-09-26 21:55:44 -0400

    rss

--- src/main/java/com/github/dimafeng/namsterdam/config/WebMvcConfig.java
+++ src/main/java/com/github/dimafeng/namsterdam/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.github.dimafeng.namsterdam.config;
 
+import com.github.dimafeng.namsterdam.controller.CustomRssViewer;
 import com.lyncode.jtwig.mvc.JtwigViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,18 +8,31 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
 
 @EnableWebMvc
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ViewResolver viewResolver() {
-
         JtwigViewResolver viewResolver = new JtwigViewResolver();
         viewResolver.setEncoding("UTF8");
         viewResolver.setPrefix("classpath:/views/");
         viewResolver.setSuffix(".html");
+        viewResolver.setOrder(1);
         return viewResolver;
+    }
+
+    @Bean
+    public ViewResolver rssViewResolver() {
+        BeanNameViewResolver viewResolver = new BeanNameViewResolver();
+        viewResolver.setOrder(0);
+        return viewResolver;
+    }
+
+    @Bean(name = "rssViewer")
+    public CustomRssViewer rssViewer() {
+        return new CustomRssViewer();
     }
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
---
+++ src/main/java/com/github/dimafeng/namsterdam/controller/CustomRssViewer.java
@@ -0,0 +1,56 @@
+package com.github.dimafeng.namsterdam.controller;
+
+import com.github.dimafeng.namsterdam.model.Article;
+import com.sun.syndication.feed.rss.Channel;
+import com.sun.syndication.feed.rss.Item;
+import org.springframework.web.servlet.view.feed.AbstractRssFeedView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CustomRssViewer extends AbstractRssFeedView {
+
+    public CustomRssViewer() {
+        super();
+        setContentType("application/rss+xml; charset=utf-8");
+    }
+
+    @Override
+    protected void buildFeedMetadata(Map<String, Object> model, Channel feed,
+                                     HttpServletRequest request) {
+
+        feed.setTitle("Новый Амстердам");
+        feed.setDescription("Наш блог – это наш собственный опыт проживания в одном из самых знаменитых городов мира – в Нью-Йорке");
+        feed.setLink("http://namsterdam.us/");
+
+        super.buildFeedMetadata(model, feed, request);
+    }
+
+
+    @Override
+    protected List<Item> buildFeedItems(Map<String, Object> model,
+                                        HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+
+        @SuppressWarnings("unchecked")
+        List<Article> listContent = (List<Article>) model.get("feedContent");
+        List<Item> items = new ArrayList<>(listContent.size());
+
+        for (Article article : listContent) {
+
+            Item item = new Item();
+            item.setContent(item.getContent());
+
+            item.setTitle(article.getTitle());
+            item.setLink("http://namsterdam.us/article/" + article.getUrlTitle());
+            item.setPubDate(article.getDisplayDate());
+
+            items.add(item);
+        }
+
+        return items;
+    }
+}
