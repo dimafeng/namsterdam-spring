ad0b7945ebb004a64f6cb583eacce2300c56ad16
Dmitry Fedosov <dimafeng@gmail.com>
2015-05-19 23:15:33 -0400

    Watermark

--- src/main/java/com/github/dimafeng/namsterdam/service/ImageService.java
+++ src/main/java/com/github/dimafeng/namsterdam/service/ImageService.java
@@ -4,6 +4,7 @@ import com.github.dimafeng.namsterdam.dao.ImageRepository;
 import com.google.common.base.Preconditions;
 import org.im4java.core.CompositeCmd;
 import org.im4java.core.ConvertCmd;
+import org.im4java.core.IM4JavaException;
 import org.im4java.core.IMOperation;
 import org.im4java.process.Pipe;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.function.Consumer;
 
 @Service
@@ -28,6 +27,8 @@ public class ImageService {
     @Autowired
     private ImageRepository imageRepository;
 
+    private Path watermark;
+
     @PostConstruct
     public void init() throws IOException {
         tempDir = Paths.get(System.getProperty("java.io.tmpdir")).resolve("images");
@@ -37,22 +38,22 @@ public class ImageService {
     }
 
     public byte[] gridImage(byte[] image, int size) throws Exception {
-        return convert(image, op -> {
+        return convert(image, false, op -> {
             op.brightnessContrast(-20., -40.);
             op.resize(size);
             //op.adaptiveBlur(10.5);
         });
     }
-    
+
     public byte[] resize(byte[] image, int size) throws Exception {
-        return convert(image, op -> {
+        return convert(image, size > 399, op -> {
             op.sharpen(1.2);
             op.resize(size);
             op.quality(95.);
         });
     }
-    
-    public byte[] convert(byte[] image, Consumer<IMOperation> operations) throws Exception {
+
+    public byte[] convert(byte[] image, boolean addWatermark, Consumer<IMOperation> operations) throws Exception {
 
         ByteArrayInputStream bais = new ByteArrayInputStream(image);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -71,16 +72,48 @@ public class ImageService {
         cmd.setInputProvider(pipeIn);
         cmd.setOutputConsumer(pipeOut);
         cmd.run(op);
-        return baos.toByteArray();
+
+        byte[] imageOutput = baos.toByteArray();
+        return addWatermark ? addWatermark(imageOutput) : imageOutput;
+    }
+
+    private byte[] addWatermark(byte[] image) throws Exception {
+        Path tempFileWithImage = Files.createTempFile("image", "");
+        Files.write(tempFileWithImage, image);
+
+        Path output = Files.createTempFile("image", "");
+
+        try {
+            IMOperation op = new IMOperation();
+            op.gravity("southeast");
+            op.addImage(getWatermarkPath());
+            op.addImage(tempFileWithImage.toAbsolutePath().toString());
+            op.addImage("jpeg:" + output.toAbsolutePath().toString());
+            CompositeCmd compositeCmd = new CompositeCmd();
+            compositeCmd.run(op);
+
+            return Files.readAllBytes(output);
+        } finally {
+            Files.deleteIfExists(tempFileWithImage);
+            Files.deleteIfExists(output);
+        }
+    }
+
+    private String getWatermarkPath() throws IOException {
+        if (watermark == null || !Files.exists(watermark)) {
+            watermark = Files.createTempFile("watermark", "");
+            Files.copy(ImageService.class.getResourceAsStream("/public/images/watermark.png"), watermark, new CopyOption[]{StandardCopyOption.REPLACE_EXISTING});
+        }
+        return watermark.toAbsolutePath().toString();
     }
 
     public byte[] convertToJpgOriginalSize(byte[] image) throws Exception {
-        return convert(image, null);
+        return convert(image, false, null);
     }
 
     public byte[] getImage(int size, String id, boolean gridImage) throws Exception {
         Path folder = tempDir.resolve(Integer.toString(size));
-        Path image = folder.resolve(id + (gridImage?"grid":"") + ".jpg");
+        Path image = folder.resolve(id + (gridImage ? "grid" : "") + ".jpg");
         if (Files.exists(image)) {
             return Files.readAllBytes(image);
         } else {
@@ -90,7 +123,7 @@ public class ImageService {
             Files.createDirectories(folder);
             byte[] data = imageRepository.findOne(id).getData();
             byte[] convert;
-            if(gridImage) {
+            if (gridImage) {
                 convert = gridImage(data, size);
             } else {
                 convert = resize(data, size);
