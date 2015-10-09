7c22e3e14cc74f9cea83a80a194981281162785b
Dmitry Fedosov <dimafeng@gmail.com>
2015-10-08 22:25:37 -0400

    docker  compose

--- docker/build-remote.sh
+++ docker/build-remote.sh
@@ -1,26 +1,4 @@
-MONGO_STARTED=$(docker ps -q --filter="name=mongo")
-echo "Mongo container id: $MONGO_STARTED"
-if [ -z "$MONGO_STARTED" ];
-then
-	echo "Mongo container isn't started"
-	if [ -n "$(docker images | grep mongo)" ];
-	then
-		echo "Mongo image is being pulled"
-		docker pull mongo
-	else
-		docker images | grep mongo
-	fi
-	echo "Start container"
-	mkdir /root/db/
-	docker run -v /root/db/:/data/db/ --restart="always" --name mongo -d mongo
-fi
-NAMSTERDAM_CONT=$(docker ps -q --filter="name=namsterdam")
-echo "Namsterdam container id: $NAMSTERDAM_CONT"
-if [ -n "$NAMSTERDAM_CONT" ];
-then
-echo "Namsterdam stopping"
-docker stop --time=10 $NAMSTERDAM_CONT
-fi
+docker-compose stop
 docker rm namsterdam
 gunzip < /tmp/namsterdam.zip | docker load
-docker run --restart="always" -p 80:8080 --link mongo:mongo -d --name namsterdam  namsterdam
+docker-compose up &
--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,6 @@ IMAGE_NAME="namsterdam"
 docker build -no-cache -t $IMAGE_NAME .
 docker save namsterdam | gzip | pv > /tmp/img.zip
 scp ./build-remote.sh $REMOTE_HOST:~/build-remote.sh
+scp ./docker-compose.yml $REMOTE_HOST:~/docker-compose.yml
 scp /tmp/img.zip $REMOTE_HOST:/tmp/namsterdam.zip
 ssh $REMOTE_HOST 'chmod +x ~/build-remote.sh && ~/build-remote.sh'
